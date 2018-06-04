6de06d8250e51d0259a53be5232493bf51797488
Hidenori Fujimura <hfu@users.noreply.github.com>
2018-06-04 17:16:06 -0400

    update

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # better-lite-source
 Spatialite feature source, now with better-sqlite3
 # usage
+You need to provide sqlite file name and also relation name.
 ```
-$ node index.js some_spatialite.sqlite
+$ node index.js 0-0-0.sqlite points
 ```
 
--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const Database = require('better-sqlite3')
 const fs = require('fs')
 const wkx = require('wkx')
 
-if (process.argv.length !== 3) {
-  console.log('usage: node index.js some.sqlite')
+if (process.argv.length !== 4) {
+  console.log('usage  : node index.js some.sqlite some_relation')
+  console.log('example: node index.js 0-0-0.sqlite points')
   process.exit()
 } else {
   if (!fs.existsSync(process.argv[2])) {
@@ -13,13 +14,21 @@ if (process.argv.length !== 3) {
 }
 
 const db = new Database(process.argv[2], {readonly: true})
-const stmt = db.prepare(`SELECT * FROM points`)
+const stmt = db.prepare(`SELECT * FROM ${process.argv[3]}`)
 let count = 0
 for (let row of stmt.iterate()) {
-  const g = wkx.Geometry.parse(row.GEOMETRY)
+  const g = wkx.Geometry.parse(row.GEOMETRY).toGeoJSON()
+  delete row.GEOMETRY
   count ++
-  if (count % 100000 === 0) console.log(`${count}`)
-//  console.log(g.toGeoJSON())
-  
+  if (count % 100000 === 0) console.error(`${count}`)
+  let f = {
+    type: 'Feature',
+    geometry: g,
+    properties: row,
+    tippecanoe: {
+      layer: g.type.toLowerCase().replace('multi', '')
+    }
+  }
+  console.log(JSON.stringify(f))
 }
-console.log(count)
+console.error(count)
